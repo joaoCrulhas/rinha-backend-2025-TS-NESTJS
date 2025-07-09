bb218ba8b593ef655a8537506e4c5b0777d3d5bd
joaocrulhas <joao.crulhas@gmail.com>
2025-07-09 09:48:21 +0200

    feat: add logger

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({
+      logger: true,
+    }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
