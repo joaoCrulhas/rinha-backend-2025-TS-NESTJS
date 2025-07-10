fc3ff4917b5ea51f8a5517eab9605ad0f3cebf69
joaocrulhas <joao.crulhas@gmail.com>
2025-07-10 21:08:53 +0200

    feat: add validation to DTO create payment

--- src/modules/payments/dtos/request/create-payment-request.dto.ts
+++ src/modules/payments/dtos/request/create-payment-request.dto.ts
@@ -1,6 +1,11 @@
+import { IsDate, IsNumber, IsUUID } from 'class-validator';
+
 export class CreatePaymentRequestDto {
+  @IsUUID()
   correlationId: string;
+  @IsNumber()
   amount: number;
+  @IsDate()
   requestedAt: Date;
   constructor(correlationId: string, amount: number, requestedAt: Date) {
     this.correlationId = correlationId;
