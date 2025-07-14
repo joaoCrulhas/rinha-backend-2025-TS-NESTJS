c953f3634e64a0ee42ad66bf54b91d4ba617286a
joaocrulhas <joao.crulhas@gmail.com>
2025-07-14 22:27:02 +0200

    feat: create redis

--- src/modules/payments/processors/process-payment.processor.ts
+++ src/modules/payments/processors/process-payment.processor.ts
@@ -3,21 +3,39 @@ import { Job } from 'bullmq';
 import { CreatePaymentRequestDto } from '@payments/dtos';
 import { CreatePaymentService } from '@payments/services';
 import { Payment } from '@payments/entities';
-import { Logger } from '@nestjs/common';
+import { Inject, Logger } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
-@Processor('process-payment-queue')
+@Processor('process-payment-queue', {
+  concurrency: 10,
+  limiter: {
+    max: 100,
+    duration: 1000,
+  },
+})
+// mainProcessorStatus
+// fallbackProcessorStatus
 export class ProcessPaymentProcessor extends WorkerHost {
   private readonly logger = new Logger(ProcessPaymentProcessor.name);
-  constructor(private readonly createPaymentService: CreatePaymentService) {
+  constructor(
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
+    private readonly createPaymentService: CreatePaymentService,
+  ) {
     super();
   }
   async process(job: Job<CreatePaymentRequestDto, Payment>): Promise<any> {
+    this.logger.debug(
+      `Starting to process payment: ${JSON.stringify(job.data)}`,
+    );
     try {
-      this.logger.debug(`Processing payment: ${JSON.stringify(job.data)}`);
       return await this.createPaymentService.execute(job.data, 'default');
-    } catch (e) {
-      this.logger.error(`Error in default payment processor`);
-      throw e;
+    } catch (error) {
+      this.logger.error(
+        `Error to process payment[host='default']: ${JSON.stringify(job.data)}, ${job.id}: ${error.message}`,
+        error.stack,
+      );
+      throw error;
     }
   }
 
@@ -27,14 +45,43 @@ export class ProcessPaymentProcessor extends WorkerHost {
   }
 
   @OnWorkerEvent('failed')
-  async onFailed(job: Job<CreatePaymentRequestDto, Payment>) {
+  async onFailed(job: Job<CreatePaymentRequestDto, Payment>, error: Error) {
+    this.logger.error(
+      `Error to process the job ID: ${job.id}): ${error.message}`,
+      error.stack,
+    );
+
+    if (job.attemptsMade < (job.opts.attempts || 1)) {
+      this.logger.warn(
+        `Retrying (attempts ${job.attemptsMade + 1}/${job.opts.attempts || 1}): ${job.id}`,
+      );
+      return;
+    }
+
+    this.logger.warn(`No more attempts left, moving job to failed: ${job.id}`);
     try {
-      this.logger.log(`Retrying payment: ${JSON.stringify(job.data)}`);
-      await this.createPaymentService.execute(job.data, 'default');
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error: any) {
-      this.logger.error(`Retrying payment with fallback`);
-      await this.createPaymentService.execute(job.data, 'fallback');
+      const isMainProcessorFailing = await this.cacheManager.get<boolean>(
+        'mainProcessorStatus',
+      );
+
+      if (isMainProcessorFailing) {
+        this.logger.log(
+          `Main processor is failing. Attempting with 'fallback' service for job ${job.id}.`,
+        );
+        await this.createPaymentService.execute(job.data, 'fallback');
+      } else {
+        this.logger.log(
+          `All retries exhausted. Attempting with 'fallback' service for job ${job.id}.`,
+        );
+        await this.createPaymentService.execute(job.data, 'fallback');
+      }
+    } catch (fallbackError) {
+      await job.moveToFailed(
+        new Error('Error to process payment.'),
+        null,
+        false,
+      );
+      throw fallbackError;
     }
   }
 }
